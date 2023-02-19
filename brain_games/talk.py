9d84f409551881aa08c43a9203a06ad9d75fa4df
Cvilev Andrey <CvilevAS@mail.ru>
2023-02-19 11:18:52 +0500

    do something

--- brain_games/talk.py
+++ brain_games/talk.py
@@ -16,13 +16,13 @@ def talk_game(get_question, case):
         print("Question: {}".format(question))
         user_answer = input("Your answer: ")
         # if correct_answer() == user_answer:
-        if transform_answer(correct_answer) or correct_answer == user_answer:
+        if transform_answer(correct_answer) == user_answer:
             print("Correct!")
         else:
             print(
                 "'{}' is wrong answer ;(. Correct answer was '{}'.".format(
             #        user_answer, correct_answer))
-                     user_answer, transform_answer(correct_answer) or correct_answer))
+                     user_answer, transform_answer(correct_answer)))
             print("Let's try again, {}!".format(user_name))
             return
     print("Congratulations, {}!".format(user_name))
